fe3b29d4838293a58b12b5f550b4f46f7105d637
eBusiness security <eBus@eBusinesss-MacBook-Pro.local>
2017-04-26 15:54:03 +0300

    setup

--- ios/react_native_navigation_bootstrap/AppDelegate.m
+++ ios/react_native_navigation_bootstrap/AppDelegate.m
@@ -1,4 +1,8 @@
 #import "AppDelegate.h"
+#import <RNCrashes/RNCrashes.h>
+
+#import <RNAnalytics/RNAnalytics.h>
+
 #import "RCCManager.h"
 #import <React/RCTRootView.h>
 
@@ -7,6 +11,10 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   NSURL *jsCodeLocation;
+
+  [RNCrashes registerWithCrashDelegate: [[RNCrashesDelegateAlwaysSend alloc] init]];  // Initialize Mobile Center crashes
+
+  [RNAnalytics registerWithInitiallyEnabled:true];  // Initialize Mobile Center analytics
 #ifdef DEBUG
   jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
 #else
